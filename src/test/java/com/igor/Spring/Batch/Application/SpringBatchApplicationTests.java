bbb476332e3bb86154ebfcced2ae1bcf8926e7a1
iefn <iefn@gft.com>
2022-08-16 14:24:53 -0300

    Initial commit

---
+++ src/test/java/com/igor/Spring/Batch/Application/SpringBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.igor.Spring.Batch.Application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
